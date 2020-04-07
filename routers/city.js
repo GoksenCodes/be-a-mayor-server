0d681454b6c97b854338040d00a86d2f8e8dba6f
chasin87 <yasinyuksek87@gmail.com>
2020-04-07 15:50:32 +0200

    router.post created for add products

--- routers/city.js
+++ routers/city.js
@@ -1,18 +1,27 @@
 const { Router } = require("express");
-const City = require('../models').city;
+const City = require("../models").city;
 
 const router = new Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const allCities = await City.findAll();
   res.status(200).json(allCities);
-})
+});
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const city = await City.findOne({
-    where: { id: req.params.id},
+    where: { id: req.params.id },
   });
   res.status(200).json(city);
+});
+
+//Post add a City
+
+router.post("/", auth, async (req, res) => {
+  console.log("what is req.body", req.body);
+  const newCity = await City.create(req.body);
+  console.log(newCity.dataValues);
+  res.status(201).send({ message: "Add a City", city: newCity });
 });
 
 module.exports = router;
