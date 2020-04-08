e50f2b3aa9c0b8fd72ef1cc6151a07a367ff4d13
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-08 12:16:18 +0200

    created endpoint for filteredCities

--- routers/city.js
+++ routers/city.js
@@ -1,28 +1,60 @@
 const { Router } = require("express");
+const { Op } = require("sequelize");
 const City = require("../models").city;
 const auth = require("../auth/middleware");
 const router = new Router();
 
 router.get("/", async (req, res) => {
-  const allCities = await City.findAll();
-  res.status(200).json(allCities);
+  try {
+    const allCities = await City.findAll();
+    res.status(200).json(allCities);
+  }
+  catch (e) {
+    console.log('error: ', e);
+  }
 });
 
-console.log("hello");
+router.get("/:country/:continent/", async (req, res) => {
+  try {
+    let filteredCities;
+    if(req.params.country !== 'all' && req.params.continent === 'all') {
+      filteredCities = await City.findAll({
+        where: { 
+          country: req.params.country, 
+        }
+      });
+    } 
+    return res.status(200).json(filteredCities);
+  }
+  catch (e) {
+    console.log('error: ', e);
+  }
+})
+
+
 router.get("/:id", async (req, res) => {
-  const city = await City.findOne({
-    where: { id: req.params.id },
-  });
-  res.status(200).json(city);
+  try {
+    const city = await City.findOne({
+      where: { id: req.params.id },
+    });
+    res.status(200).json(city);
+  }
+  catch (e) {
+    console.log('error: ', e)
+  }
 });
 
 //Post add a City
 
 router.post("/addacity", auth, async (req, res) => {
-  console.log("what is req.body", req.body);
-  const newCity = await City.create(req.body);
-  console.log(newCity.dataValues);
-  res.status(201).send({ message: "Add a City", city: newCity });
+  try {
+    const newCity = await City.create(req.body);
+    console.log(newCity.dataValues);
+    res.status(201).send({ message: "Add a City", city: newCity });
+  }
+  catch (error) {
+    console.log('error: ', error);
+  }
 });
 
 module.exports = router;
